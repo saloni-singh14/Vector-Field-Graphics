da25799cb180004b27c59de7daaacbcc5bc06681
Saloni Singh <62534764+saloni-singh14@users.noreply.github.com>
2021-06-08 03:14:06 +0530

    Add files via upload

---
+++ documentation/_circle_drawing_8cpp.js
@@ -0,0 +1,17 @@
+var _circle_drawing_8cpp =
+[
+    [ "main", "_circle_drawing_8cpp.html#a70db8bd1d499619f7ff9c1ca2ff3c8df", null ],
+    [ "midPointCircleAlgo", "_circle_drawing_8cpp.html#a6807b88b6dfccda4b0414f9359576949", null ],
+    [ "my_Init", "_circle_drawing_8cpp.html#ae4d7e9f49f36c70bd1024db1435902b3", null ],
+    [ "myDisplay", "_circle_drawing_8cpp.html#ac158bfc1571fb83c31d7035c8497a5a3", null ],
+    [ "plot", "_circle_drawing_8cpp.html#aa9374430495f628f2b53a29747717129", null ],
+    [ "PlotCircle", "_circle_drawing_8cpp.html#a588da0321d63a6bfac2d37e5bebb36b9", null ],
+    [ "B", "_circle_drawing_8cpp.html#a355b77268c28b4917409c2f3852df921", null ],
+    [ "G", "_circle_drawing_8cpp.html#a076737452f907497bc04aa77a9157234", null ],
+    [ "pntX1", "_circle_drawing_8cpp.html#acb5014558c8ac807745efad1a0c6e4e7", null ],
+    [ "pntY1", "_circle_drawing_8cpp.html#a79006f62443558ba76303461a097a15a", null ],
+    [ "r", "_circle_drawing_8cpp.html#acab531abaa74a7e664e3986f2522b33a", null ],
+    [ "R", "_circle_drawing_8cpp.html#ad14768b32b01137f2cdb95adc96d8da8", null ],
+    [ "screenX", "_circle_drawing_8cpp.html#a76395f2680b21afc2034736269f644e8", null ],
+    [ "screenY", "_circle_drawing_8cpp.html#a8044665d55214c345872bf2b79d8e270", null ]
+];
---
+++ documentation/_line_drawing_8cpp.js
@@ -0,0 +1,24 @@
+var _line_drawing_8cpp =
+[
+    [ "Bresenham", "_line_drawing_8cpp.html#a83237f622961bcd9ff98ad0475e80541", null ],
+    [ "main", "_line_drawing_8cpp.html#a70db8bd1d499619f7ff9c1ca2ff3c8df", null ],
+    [ "my_Init", "_line_drawing_8cpp.html#ae4d7e9f49f36c70bd1024db1435902b3", null ],
+    [ "MyDisplay", "_line_drawing_8cpp.html#a02a6afe113dae05cb76b43f0484169cf", null ],
+    [ "plot", "_line_drawing_8cpp.html#aa9374430495f628f2b53a29747717129", null ],
+    [ "Simplify", "_line_drawing_8cpp.html#a05358db3d174a16ec78a2ad5a8c6aac1", null ],
+    [ "swap", "_line_drawing_8cpp.html#aa23258ab9773977c9cb5176081178d62", null ],
+    [ "B", "_line_drawing_8cpp.html#a355b77268c28b4917409c2f3852df921", null ],
+    [ "G", "_line_drawing_8cpp.html#a076737452f907497bc04aa77a9157234", null ],
+    [ "R", "_line_drawing_8cpp.html#ad14768b32b01137f2cdb95adc96d8da8", null ],
+    [ "scale", "_line_drawing_8cpp.html#a1d28dec57cce925ad92342891bd71e7c", null ],
+    [ "screenX", "_line_drawing_8cpp.html#a76395f2680b21afc2034736269f644e8", null ],
+    [ "screenY", "_line_drawing_8cpp.html#a8044665d55214c345872bf2b79d8e270", null ],
+    [ "X1", "_line_drawing_8cpp.html#a93422739141d62735190fb61dbb96d20", null ],
+    [ "X2", "_line_drawing_8cpp.html#adb6b7eb8d3396f10e35715b3919268ee", null ],
+    [ "xMax", "_line_drawing_8cpp.html#af201ccbf3fe6a7e8274aa3eca22ac711", null ],
+    [ "xMin", "_line_drawing_8cpp.html#aa91afe2b50db2f2fc786f4caf9f16f69", null ],
+    [ "Y1", "_line_drawing_8cpp.html#a4b2a145eebf87b1b0879455628375e97", null ],
+    [ "Y2", "_line_drawing_8cpp.html#a242126254d0b287fb37fe31b97bc3498", null ],
+    [ "yMax", "_line_drawing_8cpp.html#a7ca443cbb568e95510880b6ec54dbe5e", null ],
+    [ "yMin", "_line_drawing_8cpp.html#abd0259c29e89b8f4ee318478bf207cf8", null ]
+];
